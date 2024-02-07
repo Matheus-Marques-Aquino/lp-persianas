10215d941f56c5e5006f82cebe6db664c34ced0f
Matheus Marques <matheus.marques.aquino@gmail.com>
2024-02-07 00:38:50 -0300

    Update HeaderMessage.js

--- components/HeaderMessage.js
+++ components/HeaderMessage.js
@@ -1,8 +1,14 @@
 export default function Form() {
     return (
-        <div className="text-[#313131] text-left max-w-[620px]">
-            <div className="max-w-[450px] text-[35px] font-semibold leading-[35px]" >
+        <div className="text-[#313131] text-left max-w-[490px]">
+            <div className="max-w-[450px] text-[35px] font-semibold leading-[35px] tracking-[0.025rem]" >
                 São diversas opções de persianas sob medida
+            </div>
+            <div className="text-[16px] font-normal leading-[20px] mt-[15px]">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </div>
+            <div className="py-[8px] px-[30px] w-[190px] bg-[#3EC263] rounded mt-[20px] font-medium text-white text-[16px] cursor-pointer hover:opacity-9 flex transition ease-in-out duration-300">
+                Ver modelos
             </div>
         </div>
     );
